aff14dc6734f66d63bc40929ebab15b7ceb96ca9
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-05-20 21:54:26 -0500

    feat(Infraestructure): added deliver constructor

--- Infraestructure/Models/Deliver.cs
+++ Infraestructure/Models/Deliver.cs
@@ -7,3 +7,14 @@ public partial class Deliver
     public DateTime Date { get; set; }
     public double Amount { get; set; }
 }
+
+public partial class Deliver
+{
+    public Deliver()
+    {
+        this.Id = 0;
+        this.ClientId = 0;
+        this.Date = DateTime.Now;
+        this.Amount = 0;
+    }
+}
